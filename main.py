29f3ba97a0cc0594f9de5292f7e1e1db5986803e
RickLee910 <13554543910@163.com>
2021-03-30 10:37:15 +0800

    yes

---
+++ main.py
@@ -0,0 +1,20 @@
+import torch.nn as nn
+import numpy as np
+import math
+from torch.autograd import Variable
+
+def import_class(name):
+    components = name.split('.')
+    mod = __import__(components[0])
+    for comp in components[1:]:
+        mod = getattr(mod, comp)
+    return mod
+
+def conv_banch_init(conv, branches):
+    weight = conv.weight
+    n = weight.size(0)
+    k1 = weight.size(1)
+    k2 = weight.size(2)
+    nn.init.normal_(weight, 0, math.sqrt(2. / (n * k1 * k2 * branches))) #normal distribution
+    nn.init.constant_(conv.bias, 0) #init to a constant
+
